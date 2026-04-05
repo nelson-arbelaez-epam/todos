5c25a38c8102d5a1cbb2bad639f2fac569940adc
Nelson Arbelaez <nelson_arbelaez@epam.com>
2026-04-05 00:36:01 -0500

    Adopt shared favicon across web and mobile

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,27 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import sharedFaviconUrl from '../../../packages/shared/assets/todos-favicon.svg?url';
 import './index.css';
 import App from './App.tsx';
 
-createRoot(document.getElementById('root')!).render(
+const existingFavicon = document.querySelector<HTMLLinkElement>("link[rel='icon']");
+if (existingFavicon) {
+  existingFavicon.href = sharedFaviconUrl;
+} else {
+  const favicon = document.createElement('link');
+  favicon.rel = 'icon';
+  favicon.type = 'image/svg+xml';
+  favicon.href = sharedFaviconUrl;
+  document.head.appendChild(favicon);
+}
+
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  throw new Error('Root element not found');
+}
+
+createRoot(rootElement).render(
   <StrictMode>
     <BrowserRouter>
       <App />
